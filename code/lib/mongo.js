6ac9646b5c68f3fc05a920d204ee9b845970315d
lianghaixing <1772398817@qq.com>
2017-05-14 09:46:55 +0800

    Update mongo.js

--- code/lib/mongo.js
+++ code/lib/mongo.js
@@ -1,28 +1,10 @@
-var config = require('config-lite')(__dirname);
+var config = require('config-lite');
 var Mongolass = require('mongolass');
 var mongolass = new Mongolass();
-var moment = require('moment');
-var objectIdToTimestamp = require('objectid-to-timestamp');
-
 mongolass.connect(config.mongodb);
 
-exports.User = mongolass.model('User', {
-  name: { type: 'string' },
-  password: { type: 'string' },
-  avatar: { type: 'string' },
-  gender: { type: 'string', enum: ['m', 'f', 'x'] },
-  bio: { type: 'string' }
-});
-exports.User.index({ name: 1 }, { unique: true }).exec();// 根据用户名找到用户，用户名全局唯一
-
-exports.Course = mongolass.model('Course', {
-  name: { type: 'string' },
-  manager: { type: 'string'},
-  stulist: { type: 'string'},
-  bio: { type: 'string'}
-});
-exports.Course.index({ name: 1 }, { unique: true }).exec();  // 课程名全局唯一
-
+var moment = require('moment');
+var objectIdToTimestamp = require('objectid-to-timestamp');
 
 // 根据 id 生成创建时间 created_at
 mongolass.plugin('addCreatedAt', {
@@ -40,6 +22,15 @@ mongolass.plugin('addCreatedAt', {
   }
 });
 
+exports.User = mongolass.model('User', {
+  name: { type: 'string' },
+  password: { type: 'string' },
+  avatar: { type: 'string' },
+  gender: { type: 'string', enum: ['m', 'f', 'x'] },
+  bio: { type: 'string' }
+});
+exports.User.index({ name: 1 }, { unique: true }).exec();// 根据用户名找到用户，用户名全局唯一
+
 exports.Post = mongolass.model('Post', {
   author: { type: Mongolass.Types.ObjectId },
   title: { type: 'string' },
@@ -47,3 +38,11 @@ exports.Post = mongolass.model('Post', {
   sit: { type: 'string' }
 });
 exports.Post.index({ author: 1, _id: -1 }).exec();// 按创建时间降序查看用户的签到记录
+
+exports.Course = mongolass.model('Course', {
+  name: { type: 'string' },
+  manager: { type: 'string'},
+  stulist: { type: 'string'},
+  bio: { type: 'string'}
+});
+exports.Course.index({ name: 1 }, { unique: true }).exec();  // 课程名全局唯一
