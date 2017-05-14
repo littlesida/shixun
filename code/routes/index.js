5b6ad21326bbc78ff0da5f1c5bc5d6083977a3ab
lianghaixing <1772398817@qq.com>
2017-05-14 09:50:36 +0800

    Update index.js

--- code/routes/index.js
+++ code/routes/index.js
@@ -1,14 +1,14 @@
-module.exports = function (app) {
-  app.get('/', function (req, res) {
-    res.redirect('/signin');
-  });
-  app.use('/signup', require('./signup'));
-  app.use('/signin', require('./signin'));
-  app.use('/signout', require('./signout'));
-  app.use('/posts', require('./posts'));
-  app.use('/home', require('./home'));
-  app.use('/createCourse', require('./createCourse'));
-  // 404 page
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.redirect('/home');
+    });
+    app.use('/signup', require('./signup'));
+    app.use('/signin', require('./signin'));
+    app.use('/signout', require('./signout'));
+    app.use('/posts', require('./posts'));
+    app.use('/home', require('./home'));
+    app.use('/createCourse', require('./createCourse'));
+    // 404 page
     app.use(function(req, res) {
         if (!res.headersSent) {
             res.render('404');
