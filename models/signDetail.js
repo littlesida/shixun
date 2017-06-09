d095c41d88acffecbed75b6d821cd7925cf28807
littlestar <1289565858@qq.com>
2017-06-09 22:40:23 +0800

    添加学生签到信息model

---
+++ models/signDetail.js
@@ -0,0 +1,30 @@
+var SignDetail = require('../lib/mongo').SignDetail;
+
+
+module.exports = {
+    create: function create(signDetail) {
+    return SignDetail.create(signDetail).exec();
+  },
+  getRightItemsByCourseAndSignName: function getRightItemsByCourseAndSignName(course, sign) {
+    var query = {};
+    query.courseName = course;
+    query.signName = sign;
+    query.state = true;
+    return SignDetail
+      .find(query)
+      .sort(_id: -1)
+      .addCreateAt()
+      .exec();
+  },
+  getErrorItemsByCourseAndSignName: function getErrorItemsByCourseAndSignName(course, sign) {
+    var query = {};
+    query.courseName = course;
+    query.signName = sign;
+    query.state = false;
+    return SignDetail
+      .find(query)
+      .sort(_id: -1)
+      .addCreateAt()
+      .exec();
+  }
+};
