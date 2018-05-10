7dbd6ecc9b30f28d9756a59a0b8bdf9080b48ef6
littlesida <1289565858@qq.com>
2018-05-10 18:30:47 +0800

    修改localhost为0.0.0.0

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ if (module.parent) {
   module.exports = app;
 } else {
   // 监听端口，启动程序
-  app.listen(config.port, function () {
+  app.listen(config.port, '0.0.0.0', function () {
      console.log(`${pkg.name} listening on port ${config.port}`);
   });
 }
